556a89674e699910d890f1a6295fe88667aa3518
Isabelle Villasenor <villasenori@outlook.com>
2021-01-20 10:03:34 -0700

    Add user-story-1

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,6 @@ class Flight < ApplicationRecord
   validates_presence_of :departure_city, :arrival_city
   validates_numericality_of :flight_number
   belongs_to :airline
-  has_many :passenger_flights
+  has_many :passenger_flights, dependent: :destroy
   has_many :passengers, through: :passenger_flights
 end
